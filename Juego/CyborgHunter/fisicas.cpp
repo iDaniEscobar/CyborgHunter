27c8adef496cd1191bb2de8b26934fc9f4e5e46d
Daniela Escobar V <daniela.escobarv@udea.edu.co>
2026-06-02 11:40:09 -0500

    Configuracion de proyecto
    Se añaden también ecuaciones físicas a utilizar

---
+++ Juego/CyborgHunter/fisicas.cpp
@@ -0,0 +1,33 @@
+#include "fisicas.h"
+#include <cmath>
+
+void Fisicas::tiroParabolico(float &x, float &y, float v0, float anguloGrados, float tiempo, float xInicial, float yInicial) {
+
+    float anguloRadianes = anguloGrados * M_PI / 180.0;
+    float g = 9.8;
+
+    x = xInicial + (v0 * std::cos(anguloRadianes) * tiempo);
+    y = yInicial - (v0 * std::sin(anguloRadianes) * tiempo) + (0.5 * g * tiempo * tiempo);
+}
+
+void Fisicas::resistencia(float &vx, float coeficienteFriccion, float masa, float dt) {
+    float fuerzaResistencia = -coeficienteFriccion * vx;
+    float aceleracion = fuerzaResistencia / masa;
+
+    vx += aceleracion * dt;
+}
+
+void Fisicas::choqueElastico(float &v, float restitucion) {
+    v = -v * restitucion;
+}
+
+void Fisicas::retroceso(float &elongacion, float &velocidadY, float k, float b, float masa, float dt) {
+
+    float fuerzaResorte = -k * elongacion;
+    float fuerzaAmortiguador = -b * velocidadY;
+    float fuerzaTotal = fuerzaResorte + fuerzaAmortiguador;
+    float aceleracionY = fuerzaTotal / masa;
+
+    velocidadY += aceleracionY * dt;
+    elongacion += velocidadY * dt;
+}
---
+++ Juego/CyborgHunter/fisicas.h
@@ -0,0 +1,20 @@
+#ifndef FISICAS_H
+#define FISICAS_H
+
+class Fisicas
+{
+private:
+    Fisicas();
+
+public:
+    static void tiroParabolico(float &x, float &y, float v0, float anguloGrados, float tiempo, float xInicial, float yInicial);
+
+    static void retroceso(float &elongacion, float &velocidadY, float k, float b, float masa, float dt);
+
+    static void resistencia(float &vx, float coeficienteFriccion, float masa, float dt);
+
+    static void choqueElastico(float &v, float restitucion);
+};
+
+#endif // FISICAS_H
+
